1273eefcb84cd6670d3bdbd8ac443537817a9c66
tranthaovenus <aolang69@gmail.com>
2022-11-25 22:49:36 +0700

    fix posting

--- app/controllers/employer/hrcentral/Posting.php
+++ app/controllers/employer/hrcentral/Posting.php
@@ -5,7 +5,7 @@ class Posting extends Controller
         $conn=$this->model("Job_postModel");
         $employer_id=$_SESSION["employer"]["id"];
 
-        $job_post=$conn->query("SELECT job_post.*,degree_name,position,experience_type FROM `job_post` join degree on job_degree_id=degree.id join job_position on job_position_id=job_position.id join job_experience on job_experience_id=job_experience.id where posted_by_id ='$employer_id' 
+        $job_post=$conn->query("SELECT job_post.*,degree_name,position,experience_type FROM `job_post` join degree on job_degree_id=degree.id join job_position on job_position_id=job_position.id join job_experience on job_experience_id=job_experience.id where posted_by_id ='$employer_id' and 
          status ='1' ")->fetchAll(PDO::FETCH_ASSOC);
         
         $this->data["sub_content"]["job_post"] = $job_post;
